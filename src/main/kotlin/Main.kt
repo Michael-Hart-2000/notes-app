fbfbfeaef3651840b8e9c1b76a72b4e0ff5769f1
Your Name <you@example.com>
2022-11-05 20:49:03 +0000

    -Add List Submenu

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -60,9 +60,26 @@ fun addNote(){
     }
 }
 
-fun listNotes(){
-    //logger.info { "listNotes() function invoked" }
-    println(noteAPI.listAllNotes())
+fun listNotes() {
+    if (noteAPI.numberOfNotes() > 0) {
+        val option = readNextInt(
+            """
+                  > --------------------------------
+                  > |   1) View ALL notes          |
+                  > |   2) View ACTIVE notes       |
+                  > |   3) View ARCHIVED notes     |
+                  > --------------------------------
+         > ==>> """.trimMargin(">"))
+
+        when (option) {
+            1 -> listAllNotes();
+            2 -> listActiveNotes();
+            3 -> listArchivedNotes();
+            else -> println("Invalid option entered: " + option);
+        }
+    } else {
+        println("Option Invalid - No notes stored");
+    }
 }
 
 fun updateNote() {
@@ -136,6 +153,14 @@ fun archiveNote() {
             println("Archive NOT Successful")
         }
     }
+}
+
+fun listAllNotes() {
+    println(noteAPI.listAllNotes())
+}
+
+fun listArchivedNotes() {
+    println(noteAPI.listArchivedNotes())
 }
 
 fun exitApp(){
